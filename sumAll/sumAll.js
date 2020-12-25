97f7543bcfd8d8063a0cefab530830dd0dc67eef
Prabhjot <pjsingh1359@gmail.com>
2020-12-25 10:29:37 -0800

    passed sumAll tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+//I want to have two parameters for the min and max numbers thatt ill be adding all the numbers between and including those up to;
+//loop through all the numbers from the min to the max untill they are all added up
+//I could have a counter which adds every icrement number including the max up.
 
+const sumAll = function(min, max) {
+  let sum = 0;
+
+  if(min < 0 || max < 0 || typeof min !== "number" || typeof max !== "number") {
+    return "ERROR";
+  }
+
+  if (min > max) {
+    for(let i = max; i <= min; i++) {
+      sum += i;
+    }
+  } else {
+    for(let i = min; i <= max; i++) {
+      sum += i;
+    } 
+  }
+  return sum;
 }
 
 module.exports = sumAll
