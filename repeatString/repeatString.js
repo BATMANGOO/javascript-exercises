cec3a16d28174b3f867d3e301d9941736404eada
Prabhjot <pjsingh1359@gmail.com>
2020-12-25 08:49:56 -0800

    passed repeatStrings tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let i = 1;
+  let arr = []
+  if (num < 0) return 'ERROR';
+  while (i <= num) {
+    arr.push(str);
+    i++
+  }
+  return arr.join('');
 }
 
 module.exports = repeatString
