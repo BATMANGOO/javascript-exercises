d1a37012dd8f6379cc294e95257832a83b1ac373
Prabhjot <pjsingh1359@gmail.com>
2020-12-25 22:36:55 -0800

    passed tempConversion tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let conversion = (f - 32) * 5/9;
+  return Math.round(conversion * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+ let conversion = (c * 9/5) + 32;
+ return Math.round(conversion * 10) / 10;
 }
 
 module.exports = {
