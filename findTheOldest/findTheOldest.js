7d9eb47003b5561bddf89ced91f3d03979abb8db
Prabhjot <pjsingh1359@gmail.com>
2021-01-08 08:40:20 -0800

    findTheOldest tests pass

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+  return people.sort((a,b) => {
+    if(!a.yearOfDeath) {
+      a.yearOfDeath = new Date().getFullYear();
+    } else if (!b.yearOfDeath) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1;
+  })[0];
 }
 
 module.exports = findTheOldest
