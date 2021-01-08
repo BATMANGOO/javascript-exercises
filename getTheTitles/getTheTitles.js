0d1d977cb57e37971fbfee174ac0660f7bf2666a
Prabhjot <pjsingh1359@gmail.com>
2021-01-08 08:09:05 -0800

    getTheTitles tests pass

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let arr = [];
+  books.forEach(book => arr.push(book.title));
+  return arr;
 }
 
 module.exports = getTheTitles;
