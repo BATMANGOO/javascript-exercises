2cf332fe303a5a2b821876a9b3093d3bf882bc78
Prabhjot <pjsingh1359@gmail.com>
2021-01-08 07:23:30 -0800

    fibonacci tests pass

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num < 0) return 'OOPS';
+  let a = 1;
+  let b = 0;
+  let nxtNum;
+  while(num > 0) {
+    nxtNum = +a;
+    a = (+a) + (+b);
+    b = +nxtNum;
+    num--
+  }
+  return nxtNum;
 }
 
 module.exports = fibonacci
